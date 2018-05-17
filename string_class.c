8cd1269b99a30efeb63bfa7c9f84d2a8b258c74f
epoch <epoch@hacking.allowed.org>
2018-05-17 01:17:20 -0500

    redid some of the string_class code to be more generalized and require less hand-copying between files.

--- string_class.c
+++ string_class.c
@@ -1,49 +1,51 @@
 #include"strglob.h"
 #include"string_class.h"
 
+//sclnm is the name of the string class we want it loops like. stuff like "wdays".
+//I'm probably going to do some hash table and initialize it on the first lookup
+//urglb is a structure that contains a pointer that we need to point at space that contains an array of strings.
+//do not know if it gets
 int string_class(const char *sclnm, STR_GLOB *urglb) {
   int invalid_class = 1;
+  int len;
 
-  for(register char **snp = stype_strs;*snp;++snp) {
-    if(!strcmp(*snp, sclnm)) {
+//  for(register char **snp = stype_strs;*snp;++snp) {//is this really needed? we do manual checks and compares later.
+//    if(!strcmp(*snp, sclnm)) {
+//      invalid_class = 0;
+//
+//      break;
+//    }
+//  }
+  int i;
+  for(i=0;derp[i].name;i++) {
+    if(!strcmp(derp[i].name,sclnm)) {
       invalid_class = 0;
-
-      break;
+      break;//we can reuse this 'i' later.
     }
   }
 
   if(invalid_class)
     return 1;
 
-  urglb->type = 5;
+  urglb->type = 5; //I bet this is just to set to 5 to say "we're a string class" to other parts of the program.
 
-  if(!strcmp(sclnm, "wdays")) {
-    urglb->out = malloc(8 * sizeof *(urglb->out));
+  register char **wp=0;
+  wp=derp[i].strs;
+  if(!wp) return 1;//invalid class.
+  for(len=0;wp[len];len++);
 
-    if(!urglb->out)
-      exit_verbose("malloc", __FILE__, __LINE__);
+//  fprintf(stderr,"len: %d\n",len);
+    
+  urglb->out = malloc((len+1) * sizeof *(urglb->out));
 
-    register char **pp = urglb->out;
-    register char **wp = wdays_strs;
+  if(!urglb->out)
+    exit_verbose("malloc", __FILE__, __LINE__);
 
-    while(*wp)
-      *pp++ = *wp++;
+  register char **pp = urglb->out;
 
-    pp[7] = NULL;
-  } else if(!strcmp(sclnm, "Wdays")) {
-    urglb->out = malloc(8 * sizeof *(urglb->out));
-
-    if(!urglb->out)
-      exit_verbose("malloc", __FILE__, __LINE__);
-
-    register char **pp = urglb->out;
-    register char **wp = Wdays_strs;
-
-    while(*wp)
-      *pp++ = *wp++;
-
-    pp[7] = NULL;
-  }
+  while(*wp)
+    *pp++ = *wp++;
+  *pp++ = NULL;
 
   return 0; 
 }
--- string_class.h
+++ string_class.h
@@ -1,26 +1,36 @@
 #ifndef _STRING_CLASS_H
 #define _STRING_CLASS_H
 
+// HACK: that 64 down there in char *strs[64].
+// not sure of the best way to get rid of it right now.
+// it will use up more space than needed... or I could make the string class config uglier.
+// will try to think of a better way.
+// this way at least prevents having to copy a list of class names between two different spots by hand.
+
+#define MAX_STRINGS_PER_CLASS 16 //this can be tuned to lowest needed.
+
 typedef struct _string_groups {
   const char *name;
-  char **strs;
   int opts;
+  char *strs[MAX_STRINGS_PER_CLASS];//if this isn't flexible it works a lot nicer. but it also takes up at least this much space for each structure.
 } STRING_GROUPS;
 
 /* if the string class ends in a 'z', then don't include the weekend days */
-static char *stype_strs[] = { "wdays", "Wdays", "WDAYS", "weekdays", "Weekdays", "WEEKDAYS", "wdayz", "Wdayz", "WDAYZ", "weekdayz", "Weekdayz", "WEEKDAYZ" };
-static char *wdays_strs[] = { "sun", "mon", "tue", "wed", "thu", "fri", "sat", NULL };
-static char *wdayz_strs[] = { "mon", "tue", "wed", "thu", "fri", NULL };
-static char *Wdayz_strs[] = { "Mon", "Tue", "Wed", "Thu", "Fri", NULL };
-static char *WDAYZ_strs[] = { "MON", "TUE", "WED", "THU", "FRI", NULL };
-static char *Wdays_strs[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", NULL };
-static char *WDAYS_strs[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT", NULL };
-static char *weekdays_strs[] = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", NULL };
-static char *Weekdays_strs[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", NULL };
-static char *WEEKDAYS_strs[] = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", NULL };
-static char *weekdayz_strs[] = { "monday", "tuesday", "wednesday", "thursday", "friday", NULL };
-static char *Weekdayz_strs[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", NULL };
-static char *WEEKDAYZ_strs[] = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", NULL };
+STRING_GROUPS derp[] = {
+ { .name="wdays", .opts=0, .strs = { "sun", "mon", "tue", "wed", "thu", "fri", "sat", NULL }},
+ { .name="wdayz", .opts=0, .strs = { "mon", "tue", "wed", "thu", "fri", NULL }},
+ { .name="Wdayz", .opts=0, .strs = { "Mon", "Tue", "Wed", "Thu", "Fri", NULL }},
+ { .name="WDAYZ", .opts=0, .strs = { "MON", "TUE", "WED", "THU", "FRI", NULL }},
+ { .name="Wdays", .opts=0, .strs = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", NULL }},
+ { .name="WDAYS", .opts=0, .strs = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT", NULL }},
+ { .name="weekdays", .opts=0, .strs = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", NULL }},
+ { .name="Weekdays", .opts=0, .strs = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", NULL }},
+ { .name="WEEKDAYS", .opts=0, .strs = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", NULL }},
+ { .name="weekdayz", .opts=0, .strs = { "monday", "tuesday", "wednesday", "thursday", "friday", NULL }},
+ { .name="Weekdayz", .opts=0, .strs = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", NULL }},
+ { .name="WEEKDAYZ", .opts=0, .strs = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", NULL }},
+ { .name=NULL, .opts=0, .strs={NULL}} //end marker
+};
 
 /* mons, Mons, MONS, months, Months, MONTHS */
 
